c82cabed805c80f33f9ccbc38cda6b7dfc45f9e0
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-12-21 17:03:01 +0430

    comments removed

--- App.jsx
+++ App.jsx
@@ -14,22 +14,6 @@ export default function App() {
 	})
 	
 	const [verified, setVerified] = React.useState(undefined)
-
-/* Challenge
-
-	The verification code form doesn't yet check the user's input. Your job is to finish setting it up as follows: 
-	
-		1. When the user types a character in one of the password inputs, the corresponding 
-		   property of the userInput state object should be updated, while preserving the other properties. Note that the property names and the names of the inputs match each other (charOne, charTwo, etc.)
-		   
-		2. When the user clicks the submit button, a submit handling function should prevent the 
-		   page from refreshing and check if the combination of the four characters stored in userInput equals the passCode value (declared on line 7 above).
-		   
-		3. If they match, the verified state value should be set to true. Otherwise, it should 
-		   be set to false. 
-		   
-		4. Your code should be as DRY as possible!
-*/
 	function handleChange(event) {
 		setUserInput(prev => ({...prev, [event.target.name]: event.target.value }))
 	}
